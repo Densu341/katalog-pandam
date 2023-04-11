fdb8c0333a507c563ff4b0d878f3994d4a210bc4
Deni Irawan <denyirawan341@gmail.com>
2023-04-12 03:34:33 +0700

    first front end

--- application/controllers/Katalog.php
+++ application/controllers/Katalog.php
@@ -7,13 +7,27 @@ class Katalog extends CI_Controller
 	{
 		parent::__construct();
 		$this->load->model('M_category');
+		$this->load->model('M_subcategory');
+		$this->load->model('M_product');
 	}
 
 	function index()
 	{
+		$data['category'] = $this->M_category->get_category();
+		$data['subcategory'] = $this->M_subcategory->get_subcategory();
 		$data['title'] = "Katalog Pandam";
 		$this->load->view('template/header', $data);
 		$this->load->view('katalog', $data);
+		var_dump($data);
+		$this->load->view('template/footer');
+	}
+
+	function product()
+	{
+		$data['product'] = $this->M_product->get_product();
+		$data['title'] = "Katalog Pandam";
+		$this->load->view('template/header', $data);
+		$this->load->view('product', $data);
 		$this->load->view('template/footer');
 	}
 }
