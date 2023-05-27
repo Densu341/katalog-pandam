802f76ea95ded8166f13ef197d733c8633f4f990
ryn-crypto <riyanfirst13@gmail.com>
2023-05-27 22:54:41 +0700

    memperbaiki report product pdf

--- application/libraries/pdf.php
+++ application/libraries/pdf.php
@@ -1,16 +1,4 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed');
-/**
- * CodeIgniter DomPDF Library
- *
- * Generate PDF's from HTML in CodeIgniter
- *
- * @packge        CodeIgniter
- * @subpackage        Libraries
- * @category        Libraries
- * @author        Ardianta Pargo
- * @license        MIT License
- * @link        https://github.com/ardianta/codeigniter-dompdf
- */
 
 use Dompdf\Dompdf;
 
@@ -46,11 +34,12 @@ class Pdf extends Dompdf
      */
     public function load_view($view, $data = array())
     {
+        // PDF::setOptions(['isHtml5ParserEnabled' => true, 'isRemoteEnabled' => true]);
+
         $html = $this->ci()->load->view($view, $data, TRUE);
         $this->load_html($html);
         // Render the PDF
         $this->render();
-        // ob_end_clean();
         // Output the generated PDF to Browser
         $this->stream($this->filename, array("Attachment" => false));
     }
--- application/views/pdf_report.php
+++ application/views/pdf_report.php
@@ -7,14 +7,14 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <!-- Bootstrap CSS -->
-  <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
   <title>Laporan Data Produk</title>
 </head>
 
 <body>
   <div class="container-fluid">
-    <div class="section text-center mt-3">
+    <div class="section text-center mt-3 border-bottom">
       <div class="row">
         <div class="col-2">
           <img src="<?= base_url() ?>/assets/favicon.png" alt="test">
@@ -26,49 +26,49 @@
         </div>
       </div>
     </div>
-    <hr size="10px">
+    <!-- <hr size="10px"> -->
     <hr>
-    <div class="section m-5">
+    <div class="section">
       <?php foreach ($produk as $data) : ?>
-        <table class="table mt-4 table-bordered">
+        <table class="table mt-3 table-bordered border">
           <tbody>
             <tr>
-              <th colspan="4" class="text-center fw-bolder"><?= $data['category_name'] ?></th>
+              <th colspan="4" class="text-center fw-bolder border"><?= $data['category_name'] ?></th>
             </tr>
             <tr>
-              <td rowspan="7" width="300px" class="text-center"><img src="<?= base_url('assets/img/subcategory/') . $data['image'] ?>" alt="produk category" width="200"></td>
+              <td rowspan="7" width="200px" class="text-center border"><img src="<?= base_url('assets/img/subcategory/') . $data['image'] ?>" alt="produk category" width="150"></td>
               <td colspan="2" class="text-center fw-bold">Spesifikasi Produk</td>
             </tr>
             <tr>
-              <td width="300px">Nama Produk</td>
-              <td><?= $data['subcategory_name'] ?></td>
+              <td class="border ml-1" width="250px">Nama Produk</td>
+              <td class="border ml-1"><?= $data['product_name'] ?></td>
             </tr>
             <tr>
-              <td>unicode</td>
-              <td>unicode</td>
+              <td class="border ml-1">unicode</td>
+              <td class="border ml-1"><?= $data['sub_code'] ?>-<?= $data['mat_code'] ?>-<?= $data['sub_code'] ?></td>
             </tr>
             <tr>
-              <td>Bahan / Material</td>
-              <td>Bahan / Material</td>
+              <td class="border ml-1">Bahan / Material</td>
+              <td class="border ml-1"><?= $data['material_name'] ?></td>
+            </tr>
+
+            <tr>
+              <td class="border ml-1">Ukuran</td>
+              <td class="border ml-1"> P.<?= $data['length'] ?>cm x L.<?= $data['width'] ?>cm x t.<?= $data['height'] ?>cm</td>
             </tr>
             <tr>
-              <td>Deskripsi</td>
-              <td>Deskripsi</td>
+              <td class="border ml-1">Harga</td>
+              <td class="border ml-1">Rp. <?= number_format($data['price'], 2, ',', '.'); ?></td>
             </tr>
             <tr>
-              <td>Ukuran</td>
-              <td>Ukuran</td>
-            </tr>
-            <tr>
-              <td>Harga</td>
-              <td>Harga</td>
+              <td class="border ml-1">Deskripsi</td>
+              <td class="border ml-1"><?= $data['description'] ?></td>
             </tr>
           </tbody>
         </table>
       <?php endforeach; ?>
     </div>
   </div>
-  <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> -->
 </body>
 
 </html>
