8e5737e5f4ebcbb9f471400aea5063197f3e549d
Deni Irawan <denyirawan341@gmail.com>
2023-07-01 04:01:48 +0700

    integrasi front end

--- application/views/user/subcategory_.php
+++ application/views/user/subcategory_.php
@@ -1,62 +1,77 @@
-<section class="container-fluid pt-2">
-  <?php foreach ($subcategory as $s) : ?>
-    <div id="<?= $s['sub_code'] ?>" class="carousel slide" data-bs-ride="carousel">
+<section class="container-fluid">
+  <?php foreach ($subcategory as $value) : ?>
+    <div id="<?= $value['sub_code'] ?>" class="carousel slide" data-bs-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <?php foreach ($product as $data) :
+          $id = 0;
+        ?>
+          <?php if ($data['sub_id'] == $value['sub_id']) : ?>
+            <?php if ($id == 0) : ?>
+              <li data-bs-target="#<?= $value['sub_code'] ?>" data-bs-slide-to="<?= $id ?>" class="active"></li>
+            <?php else : ?>
+              <li data-bs-target="#<?= $value['sub_code'] ?>" data-bs-slide-to="<?= $id ?>"></li>
+            <?php endif; ?>
+          <?php endif; ?>
+          <!-- Add more indicators if needed -->
+        <?php $id += 1;
+        endforeach; ?>
+      </ol>
+
       <!-- Slides -->
       <div class="carousel-inner">
-        <?php $firstItem = true; ?>
-        <?php foreach ($products as $p) : ?>
-          <?php if ($p['sub_id'] == $s['sub_id']) : ?>
-            <div class="carousel-item <?php if ($firstItem) {
-                                        echo 'active';
-                                        $firstItem = false;
-                                      } ?>">
-              <div class="row align-items-center mb-2 py-2">
-                <div class="col-md-6 mx-auto">
-                  <div class="sub-image text-center">
-                    <img src="<?= base_url() ?>/assets/img/product/<?= $p['picture'] ?>" alt="" class="img-fluid rounded" />
+        <?php foreach ($product as $key => $data) : ?>
+          <?php if ($data['sub_id'] == $value['sub_id']) : ?>
+            <?php if ($key == 0) : ?>
+              <div class="carousel-item active">
+                <div class="row align-items-center mb-2 py-2">
+                  <div class="col-md-6 mx-auto">
+                    <a href="" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                      <div class="sub-image text-center">
+                        <img src="<?= base_url() ?>/assets/static/<?= $data['picture'] ?>" alt="" class="img-fluid rounded" />
+                      </div>
+                    </a>
                   </div>
                 </div>
               </div>
-            </div>
+            <?php else : ?>
+              <div class="carousel-item">
+                <!-- Add content for the second slide -->
+                <div class="row align-items-center mb-2 py-2">
+                  <div class="col-md-6 mx-auto">
+                    <a href="" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                      <div class="sub-image text-center">
+                        <img src="<?= base_url() ?>/assets/static/WIGUNA_ToteBags_mix_Shibori.jpeg" alt="" class="img-fluid rounded" />
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            <?php endif; ?>
           <?php endif; ?>
         <?php endforeach; ?>
       </div>
 
       <!-- Controls -->
-      <?php if ($firstItem == false) : ?>
-        <a class="carousel-control-prev control-color" href="#<?= $s['sub_code'] ?>" role="button" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </a>
-        <a class="carousel-control-next control-color" href="#<?= $s['sub_code'] ?>" role="button" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </a>
-      <?php endif; ?>
-    </div>
-    <!-- button open modal per subcategory -->
-    <div class="row">
-      <div class="col-md-6 mx-auto">
-        <div class="text-end">
-          <button type="button" class="btn btn-outline-dark" data-bs-toggle="modal" data-bs-target="#exampleModal<?= $product['sub_id']; ?>">
-            <?= $s['subcategory_name'] ?>
-          </button>
-        </div>
-      </div>
-    </div>
-    <!-- divider -->
-    <div class="row">
-      <div class="col-md-12">
-        <hr class="my-5" />
-      </div>
+      <?php foreach ($product as $key => $data) : ?>
+        <?php if ($data['sub_id'] == $value['sub_id']) : ?>
+          <?php if ($key == 0) : ?>
+            <a class="carousel-control-prev control-color" href="#<?= $value['sub_code'] ?>" role="button" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </a>
+            <a class="carousel-control-next control-color" href="#<?= $value['sub_code'] ?>" role="button" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </a>
+          <?php endif; ?>
+        <?php endif; ?>
+      <?php endforeach; ?>
     </div>
   <?php endforeach; ?>
-</section>
 
-
-<!-- Modal -->
-<?php foreach ($product as $product) : ?>
-  <div class="modal fade" id="exampleModal<?= $product['sub_id']; ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <!-- Modal -->
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-xl">
       <div class="modal-content">
         <div class="modal-header">
@@ -197,4 +212,3 @@
       </div>
     </div>
   </div>
-<?php endforeach; ?>
